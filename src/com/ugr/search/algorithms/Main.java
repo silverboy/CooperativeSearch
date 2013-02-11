3b26e71a106404a229edaeb58b71007013a58612
silverboy <jplataj@yahoo.es>
2013-02-12 00:18:51 +0100

    Print(soy javi) in main

--- src/com/ugr/search/algorithms/Main.java
+++ src/com/ugr/search/algorithms/Main.java
@@ -13,6 +13,7 @@ public class Main {
         if (args.length < 1) {
             System.out.println("Wrong parameters.");
         } else {
+            System.out.print("Soy javi");
             params.put(Params.DEPTH, 1);
             params.put(Params.TABU_METHOD, 2);
             readParams(args);
