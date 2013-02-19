03ee122c5f9cd2c8f0c055254d2822b3d80e9a2f
silverboy <jplataj@yahoo.es>
2013-02-19 05:14:46 +0100

    Funciona!!!

--- src/com/ugr/search/algorithms/tabu/TabuSearch.java
+++ src/com/ugr/search/algorithms/tabu/TabuSearch.java
@@ -106,12 +106,10 @@ public class TabuSearch extends Algorithm {
                     tabuSearchDrop(getCurrentSolution());
                     break;
             }
-            //System.out.println("Otra iteracion id: "+getInstanceNumber());
             addFitnessEvolution(getBestSolution().getFitness());
             if(isCooperative() && !fitnessEvalLimitReached()
                     && getEvaluationCount() >= (getCooperativeEval() * getCooperativeInfo().getEvaluationTime())) {
 
-                System.out.println("Paro a cooperar id: "+getInstanceNumber()+"Veces: "+getCooperativeEval());
                 incrementCooperativeEval();
                 getCooperativeInfo().changeInformation(getInstanceNumber(), getBestSolution(), getFitnessEvolution());
                 while(!getCooperativeInfo().continueExecution(getInstanceNumber())) {
@@ -121,18 +119,16 @@ public class TabuSearch extends Algorithm {
                         e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
                     }
                 }
-                System.out.println("Sigo ejecutandome id: "+getInstanceNumber());
                 if(getCooperativeInfo().solutionChange(getInstanceNumber())) {
-                    //setCurrentSolution(Knapsack.copyKnapsackItems(getCooperativeInfo().getAlgorithmSolution(getInstanceNumber()).getValue()));
-                    //setCurrentSolutionFitness(getCooperativeInfo().getAlgorithmSolution(getInstanceNumber()).getFitness());
-                    //updateBestSolutionFromCurrent();
-                    //tabuList.clearList();
+                    setCurrentSolution(Knapsack.copyKnapsackItems(getCooperativeInfo().getAlgorithmSolution(getInstanceNumber()).getValue()));
+                    setCurrentSolutionFitness(getCooperativeInfo().getAlgorithmSolution(getInstanceNumber()).getFitness());
+                    updateBestSolutionFromCurrent();
+                    tabuList.clearList();
                 }
             }
             iteration++;
         }
         getCooperativeInfo().finishExecution(getInstanceNumber(), getBestSolution());
-        System.out.println("He acabado");
     }
 
     private boolean tabuSearchAdd(int[] solution, byte uMultiplier) {
